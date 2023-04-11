5d8663edb01a4e68173bcab1af68470c8f4b6551
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-04-11 16:45:40 +0100

    Added Camera Movement
    Camera Movement and Player model

--- Assets/PlayerCam.cs
+++ Assets/PlayerCam.cs
@@ -29,5 +29,10 @@ public class PlayerCam : MonoBehaviour
         yRotation += mouseX;
 
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
